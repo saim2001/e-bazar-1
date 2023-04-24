91a6ad27f5b2db9f3d00fa3e71c48a0d24ba9bdc
maaz ali <58228528+Maaz-Ali2001@users.noreply.github.com>
2023-04-25 00:57:19 +0500

    Update urls.py

--- e_bazar_fyp/Vendor/urls.py
+++ e_bazar_fyp/Vendor/urls.py
@@ -1,1 +1,23 @@
+from django.urls import path
+from .views import vendorRegister,Product,Category
+from .decorators import user_login_required
 
+
+vendor= vendorRegister()
+product=Product()
+categories=Category()
+
+app_name = 'Vendor'
+
+urlpatterns = [
+    path('',vendor.renDashboard,name="renDashbrd"),
+    # path('l',vendor.renLogIn,name="renlogin"),
+    path('login/',vendor.logIn,name="logIn"),
+    path('vendorregister/',vendor.register,name="vendorregister"),
+    path('addproduct/',product.renselectCat,name="addproduct"),
+    path('selectcategory1/',product.selectCat,name="selectcategory"),
+    path('selectcategory2/',product.selectSubCat,name="selectsubcategory"),
+    path('selectcategory3/',product.selectLeafCat,name="leafcat"),
+    path('addpro/',product.renAddProduct,name="addpro"),
+    path('insertpro/',product.addProduct,name='insertpro'),
+]
